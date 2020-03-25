b86da635226851228465266a02982610df08bef0
Jofreylin <trabajosjofrey@hotmail.com>
2020-03-25 01:41:50 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,27 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./Presentation/home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'home-admin',
+    loadChildren: () => import('./Presentation/home-admin/home-admin.module').then( m => m.HomeAdminPageModule)
+  },
+  {
+    path: 'lista-inventario',
+    loadChildren: () => import('./Presentation/lista-inventario/lista-inventario.module').then( m => m.ListaInventarioPageModule)
+  },
+  {
+    path: 'agregar-inventario',
+    loadChildren: () => import('./Presentation/agregar-inventario/agregar-inventario.module').then( m => m.AgregarInventarioPageModule)
+  },
+  {
+    path: 'modificar-inventario',
+    loadChildren: () => import('./Presentation/modificar-inventario/modificar-inventario.module').then( m => m.ModificarInventarioPageModule)
+  },
+  {
+    path: 'modificar-inventario/:id',
+    loadChildren: () => import('./Presentation/modificar-inventario/modificar-inventario.module').then( m => m.ModificarInventarioPageModule)
+  }
 ];
 
 @NgModule({
